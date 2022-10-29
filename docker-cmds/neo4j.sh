38ec8fc072045bdeaf5fd524555aec8d64c47e02
Kiran Madabhushi <maskiran@gmail.com>
2022-10-29 10:49:19 -0700

    delete docker utils as its moved to another repo

--- docker-cmds/neo4j.sh
+++
@@ -1,9 +0,0 @@
-docker run --name neo4j \
-    -p 7474:7474 -p 7687:7687 \
-    -d \
-    -v $HOME/ctrdata/neo4j/data:/data \
-    -v $HOME/ctrdata/neo4j/logs:/logs \
-    -v $HOME/ctrdata/neo4j/import:/var/lib/neo4j/import \
-    -v $HOME/ctrdata/neo4j/plugins:/plugins \
-    --env NEO4J_AUTH=neo4j/test \
-    neo4j
