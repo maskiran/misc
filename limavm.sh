2b8c117528c2b96d085e41b124d7831a349a4bcd
Kiran Madabhushi <maskiran@gmail.com>
2025-02-07 16:33:55 -0800

    updated lima scripts

--- limavm.sh
+++ limavm.sh
@@ -1,6 +1,5 @@
 NAME=u241
 limactl create --name $NAME --tty=false template://ubuntu-24.04
 limactl start $NAME
-limactl shell $NAME curl -o setup.sh https://raw.githubusercontent.com/maskiran/misc/refs/heads/main/ubuntu-setup.sh
-limactl shell $NAME bash setup.sh
-limactl shell $NAME sudo reboot
+limactl shell --workdir . $NAME bash setup.sh
+
--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,8 +1,11 @@
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl -o $HOME/.zshrc https://raw.githubusercontent.com/maskiran/setup-mac/refs/heads/main/zshrc
-curl -o $HOME/.oh-my-zsh/custom/themes/kiran.zsh-theme https://raw.githubusercontent.com/maskiran/setup-mac/refs/heads/main/kiran.zsh-theme
-chsh -s /usr/bin/zsh
+curl -o ohmyzsh.sh -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+bash ohmyzsh.sh --unattended
+rm ohmyzsh.sh
+curl -o $HOME/.zshrc -fsSL https://raw.githubusercontent.com/maskiran/setup-mac/refs/heads/main/zshrc
+curl -o $HOME/.oh-my-zsh/custom/themes/kiran.zsh-theme -fsSL https://raw.githubusercontent.com/maskiran/setup-mac/refs/heads/main/kiran.zsh-theme
+sudo chsh -s /usr/bin/zsh $USER
 sudo reboot
+
