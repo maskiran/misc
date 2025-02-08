393549437b8f3b8de363e27195e3fbc0fed803b3
Kiran Madabhushi <maskiran@gmail.com>
2025-02-07 16:37:41 -0800

    updated limavm script

--- limavm.sh
+++ limavm.sh
@@ -1,5 +1,8 @@
 NAME=u241
-limactl create --name $NAME --tty=false template://ubuntu-24.04
+OS=ubuntu-24.04
+
+limactl create --name $NAME --tty=false template://$OS
 limactl start $NAME
+limactl shell --workdir . $NAME curl -o setup.sh -fsSL https://raw.githubusercontent.com/maskiran/misc/refs/heads/main/ubuntu-setup.sh
 limactl shell --workdir . $NAME bash setup.sh
 
