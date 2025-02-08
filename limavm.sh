a8d3f4640c8f85af97e0ff800ba5d01d9ab0b7e3
Kiran Madabhushi <maskiran@gmail.com>
2025-02-07 21:14:51 -0800

    refactor script

--- limavm.sh
+++ limavm.sh
@@ -3,6 +3,6 @@ OS=ubuntu-24.04
 
 limactl create --name $NAME --tty=false template://$OS
 limactl start $NAME
-limactl shell --workdir . $NAME curl -o setup.sh -fsSL https://raw.githubusercontent.com/maskiran/misc/refs/heads/main/ubuntu-setup.sh
+OS_SETUP_SCRIPT=https://raw.githubusercontent.com/maskiran/misc/refs/heads/main/ubuntu-setup.sh
+limactl shell --workdir . $NAME curl -o setup.sh -fsSL $OS_SETUP_SCRIPT
 limactl shell --workdir . $NAME bash setup.sh
-
